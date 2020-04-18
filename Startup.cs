451a2dddcd7ad862062e50a22b839c2b6fb9ef0f
oskar <oskar.vedel@gmail.com>
2020-04-18 13:02:14 +0200

    fedt

--- Startup.cs
+++ Startup.cs
@@ -27,11 +27,11 @@ namespace I4GUI_Assigment_2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<MyDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<MyDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
